4986eb4f27804cc777d3c7d6a5a8525dc9ac43ab
倪杰 <leo@nijiedeMacBook-Pro.local>
2023-08-02 14:44:55 +0800

    fix

--- deploy/docker-entrypoint.d/app/31-run-label-studio-init.sh
+++ deploy/docker-entrypoint.d/app/31-run-label-studio-init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-set -e ${DEBUG:+-x}
+# set -e ${DEBUG:+-x}
 
-echo >&3 "=> Run label-studio init..."
-label-studio init -q >&3
-echo >&3 "=> label-studio init completed."
+# echo >&3 "=> Run label-studio init..."
+# label-studio init -q >&3
+# echo >&3 "=> label-studio init completed."
