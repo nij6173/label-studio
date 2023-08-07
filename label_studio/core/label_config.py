77f2d49802800901145317c784d53b6e6a8bafdf
倪杰 <leo@nijiedeMBP.ctc>
2023-08-07 17:00:55 +0800

    fix bug for check_task_data_for_cloud_file_storage

--- label_studio/core/label_config.py
+++ label_studio/core/label_config.py
@@ -419,8 +419,9 @@ def check_task_data_for_cloud_file_storage(data):
     """
     if get_env("STORAGE_TYPE") == "tcs":
         for k,v in data.items():
-            if v.startswith(settings.TENCENTCOS_STORAGE.get("ROOT_PATH", "/")) or v.startswith("/") or v.startswith("data/") or v.startswith("pred/") or v.startswith("anno/"):
-                data[k] = storage_backend.presigned_url(v)
+            if type(v) == str:
+                if v.startswith(settings.TENCENTCOS_STORAGE.get("ROOT_PATH", "/")) or v.startswith("/") or v.startswith("data/") or v.startswith("pred/") or v.startswith("anno/"):
+                    data[k] = storage_backend.presigned_url(v)
     return data
 
 
